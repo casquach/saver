60b6fe39b02a6e40a17b8e4d7194455431c9ff89
Cassie Quach <cassieq120@gmail.com>
2018-03-27 16:17:14 -0400

    cleaned up files for fraudless

--- detect-button-click/extension/html/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-document.getElementById("connect-button").addEventListener('click',
-clickHandler); });
-
-function clickHandler(e) {   PopupClick('SHOW'); }
-
-function PopupClick(str) {
-  chrome.tabs.update({'url': "extension/html/connect.html"});
-}
